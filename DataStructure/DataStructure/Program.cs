459d0521d85df241add7cacb09964fd117ba6ad3
akshayb407 <akshayb407@gmail.com>
2022-07-03 15:30:26 +0530

    UC8

--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 
 namespace DataStructure
 {
-    internal class Program
+    public class Program
     {
         static void Main( string[] args)
         {
             Console.WriteLine("Hello World!");
             LinkedList<string> list = new LinkedList<string>();
             list.Add("70");
-            //list.Add("30");
+            list.Add("30");
             list.Add("56");
             Console.WriteLine(list.ToString());
-            //list.Pop(2);          
-            Console.WriteLine(list.Search("30"));
+            //Console.WriteLine(list.Search("30"));
+            list.Insert(2, "40");
             list.display();
 
         }
