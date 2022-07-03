238457658b761b97098f0a15aa5214c9bf47e388
akshayb407 <akshayb407@gmail.com>
2022-07-03 12:50:35 +0530

    UC4

--- DataStructure/DataStructure/LinkedList.cs
+++ DataStructure/DataStructure/LinkedList.cs
@@ -6,8 +6,9 @@ using System.Threading.Tasks;
 
 namespace DataStructure
 {
-    public class LinkedList<T>
+    public class LinkedList<T> 
     {
+       
         public class Node
         {
             public T data;
@@ -18,7 +19,7 @@ namespace DataStructure
             }
         }
         private Node head;
-        //correct
+       
         public bool Append(T data)
         {
             Node n = new Node(data);
@@ -36,9 +37,90 @@ namespace DataStructure
             t.next = n;
             return true;
         }
-      
+       
+        public T Pop()
+        {
+            if (head == null)
+            {
+                throw new NullReferenceException("List is Empty");
+            }
+            Node t = head, p = head;
+            while (t.next != null)
+            {
+                p = t;
+                t = t.next;
+            }
+            T obj = t.data;
+            p.next = null;
+            return obj;
+        }
+        public T peek(int ind)
+        {
+            if (head == null)
+            {
+                return (T)default;
+            }
+            if (ind == 0)
+                return head.data;
+            Node t = head;
+            while (ind > 0 && t.next != null)
+            {
+                ind--;
+                t = t.next;
+            }
+            if (ind == 0)
+                return t.data;
+            return (T)default;
+        }
         
+        public T peek()
+        {
+            if (head == null)
+                throw new NullReferenceException("empty List");
+            Node t = head, p = head;
+            while (t.next != null)
+            {
+                p = t;
+                t = t.next;
+            }
+            T obj = t.data;
+            return obj;
+        }
+        
+        public bool Search(T data)
+        {
+            if (head == null)
+                throw new NullReferenceException("empty List");
+            Node t = head;
+            while (t != null)
+            {
+                if (t.data.Equals(data))
+                    return true;
+                t = t.next;
+            }
+            return false;
+        }
+      
+        public bool IsEmpty()
+        {
+            if (head == null)
+                return true;
+            return false;
+        }
         //correct
+        public int Size()
+        {
+            if (head == null)
+                return 0;
+            Node t = head; int count = 0;
+            while (t != null)
+            {
+                count++;
+                t = t.next;
+            }
+            return count;
+        }
+       
         public bool Add(T data)
         {
             Node n = new Node(data);
@@ -51,7 +133,122 @@ namespace DataStructure
             head = n;
             return true;
         }
+      
+        public override string ToString()
+        {
+            if (head == null)
+            {
 
+                return null;
+            }
+            Node t = head;
+            string s = "";
+            while (t != null)
+            {
+                s = s + t.data + " ";
+                t = t.next;
+            }
+            return s;
+        }
+       
+        public int Index(int v, T data)
+        {
+            int count = -1;
+            if (head == null)
+                return -1;
+            Node t = head;
+            while (t != null)
+            {
+                count++;
+                if (t.data.Equals(data))
+                    return count;
+                t = t.next;
+
+            }
+            return -1;
+        }
+        public bool Insert(int ind, T data)
+        {
+            Node n = new Node(data);
+            if (ind == 0)
+            {
+                n.next = head.next;
+                head = n;
+                return true;
+            }
+            Node t = head, pre = null;
+            while (ind > 0 && t != null)
+            {
+                ind--;
+                pre = t;
+                t = t.next;
+            }
+            if (ind == 0)
+            {
+                pre.next = n;
+                n.next = t;
+                return true;
+            }
+            throw new NullReferenceException("index is not in range");
+        }
+        
+        public bool Remove(T data)
+        {
+            if (head == null)
+                throw new NullReferenceException("list is empty");
+            if (head.next == null)
+                head = null;
+            Node t = head, pre = null;
+            while (t != null)
+            {
+                if (t.data.Equals(data))
+                {
+                    pre.next = t.next;
+                    return true;
+                }
+                pre = t;
+                t = t.next;
+            }
+            return false;
+        }
+        public T Pop(int ind)
+        {
+            T obj;
+            if (ind == 0)
+            {
+                obj = head.data;
+                head = head.next;
+                return obj;
+            }
+            Node t = head, pre = null;
+            while (ind > 0 && t != null)
+            {
+                ind--;
+                pre = t;
+                t = t.next;
+            }
+
+
+            if (ind == 0)
+            {
+                pre.next = t.next;
+                obj = t.data;
+                return obj;
+            }
+            throw new NullReferenceException("index is not range");
+
+
+        }
+        public T[] ToArray()
+        {
+            int size = Size();
+            T[] array = new T[size];
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = peek(i);
+            }
+            return array;
+        }
         internal void display() //to display the program
         {
 
@@ -67,11 +264,7 @@ namespace DataStructure
                 temp = temp.next;
             }
         }
-
-
-
     }
-
 
 }
 
