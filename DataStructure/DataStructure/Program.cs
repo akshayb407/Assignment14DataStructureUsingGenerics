c0552fa5a2cc343512ae0c9749ea6947fdceff6b
akshayb407 <akshayb407@gmail.com>
2022-07-03 15:19:25 +0530

    UC7 added

--- DataStructure/DataStructure/Program.cs
+++ DataStructure/DataStructure/Program.cs
@@ -10,10 +10,11 @@ namespace DataStructure
             Console.WriteLine("Hello World!");
             LinkedList<string> list = new LinkedList<string>();
             list.Add("70");
-            list.Add("30");
+            //list.Add("30");
             list.Add("56");
-           // list.Pop(0);
-            list.Pop(2);
+            Console.WriteLine(list.ToString());
+            //list.Pop(2);          
+            Console.WriteLine(list.Search("30"));
             list.display();
 
         }
